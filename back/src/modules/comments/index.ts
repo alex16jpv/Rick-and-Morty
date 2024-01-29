11a56f644c8949ce7d8876a10e675843171fe5f8
Alexander Valencia <alex16jpv@gmail.com>
2024-01-29 08:54:02 -0500

    Add JSON server for database and update Comments module

--- back/src/modules/comments/index.ts
+++ back/src/modules/comments/index.ts
@@ -1,17 +1,25 @@
 export default class Comments {
-  comments: any[];
+  url: string;
   constructor() {
-    this.comments = [];
+    this.url = "http://localhost:3001/comments";
   }
 
   async getComments(params) {
-    return this.comments;
+    const response = await fetch(`${this.url}/comments?id=${params.id}`);
+    const result = await response.json();
+
+    return result;
   }
 
   async createComment(params) {
-    console.log(params);
+    await fetch(`${this.url}/comments`, {
+      method: "POST",
+      body: JSON.stringify(params),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-    this.comments.push(params);
-    return this.comments;
+    return this.getComments({ id: params.id });
   }
 }
